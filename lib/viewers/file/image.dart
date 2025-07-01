ed4a839a045a786a8732178934d168a9aa4b7746
Owen Garmire <owen.garmire@timu.com>
2025-07-01 14:05:06 -0700

    fix flutter analyze check

--- lib/viewers/file/image.dart
+++ lib/viewers/file/image.dart
@@ -3,7 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:interactive_viewer_2/interactive_viewer_2.dart';
 import 'package:meshagent/room_server_client.dart';
 import 'package:meshagent_flutter_shadcn/file_preview/file_preview.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 
 class ImageViewer extends StatefulWidget {
   const ImageViewer({super.key, required this.room, required this.path});
